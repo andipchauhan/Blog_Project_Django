59bd8dc70acb62ea5f87c10cc58d21874b636cd2
Andip Chauhan <chauhan2244andip@gmail.com>
2025-08-07 10:43:43 +0530

    Added URLs and view function declarations

---
+++ my_site/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index-page'),
+    path('posts', views.posts, name='posts-page'),  # /posts
+    path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),   # /posts/my-first-post
+]
