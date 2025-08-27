21a030d0bc1c5beffdfd2210ae382a5fd31593a5
Andip Chauhan <chauhan2244andip@gmail.com>
2025-08-27 13:01:30 +0530

    Managing read later via sessions

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index-page'),
     path('posts', views.AllPostsView.as_view(), name='posts-page'),  # /posts
     path('posts/<slug:slug>', views.SinglePostView.as_view(), name='post-detail-page'),   # /posts/<post_title>
+    path('read-later', views.ReadLaterView.as_view(), name='read-later')
 ]
